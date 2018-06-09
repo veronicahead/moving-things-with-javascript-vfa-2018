a615bfd1c1c600e0c92c564597af7db66df185f6
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-09 21:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const app = "I don't do much.";
 var dodger = document.getElementById('dodger');
 
 dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.left //180px
+dodger.style.bottom = '100px'
